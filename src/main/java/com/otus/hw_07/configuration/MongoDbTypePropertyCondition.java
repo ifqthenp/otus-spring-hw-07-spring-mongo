6c54f79e067959ecac636c6baf3de54de7bfaf3b
Andrei Bogomja <bogomja@gmail.com>
2019-02-22 09:07:44 +0000

    Extract hardcoded string app property to the constant

--- src/main/java/com/otus/hw_07/configuration/MongoDbTypePropertyCondition.java
+++ src/main/java/com/otus/hw_07/configuration/MongoDbTypePropertyCondition.java
@@ -9,10 +9,11 @@ import org.springframework.lang.Nullable;
 public class MongoDbTypePropertyCondition implements Condition {
 
     private static final String DB_TYPE = "mongo-prod";
+    private static final String APP_DB_TYPE = "app.dbType";
 
     @Override
     public boolean matches(@Nullable final ConditionContext context, @Nullable final AnnotatedTypeMetadata metadata) {
-        String dbType = context != null ? context.getEnvironment().getProperty("app.dbType") : null;
+        String dbType = context != null ? context.getEnvironment().getProperty(APP_DB_TYPE) : null;
         System.out.println(dbType);
         return DB_TYPE.equalsIgnoreCase(dbType);
     }
